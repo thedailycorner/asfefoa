2ad799a0fd7739851fea8fe1f7d0812cc03dbcbb
thedailycorner <thedailycorner472000@gmail.com>
2025-04-30 21:02:32 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo apt update && sudo apt install libboost-dev libssl-dev libcrypto++-dev nlohmann-json3-dev
+git clone https://github.com/Bitcoin-ABC/secp256k1.git
+cd secp256k1
+./autogen.sh
+mkdir build
+cd build
+../configure
+make -j4
+sudo make install
+cd ../..
+g++ versa_hash.cpp -O3 -fopenmp -lssl -lcrypto -L/usr/local/lib -lsecp256k1 -static -o versa
+./versa
